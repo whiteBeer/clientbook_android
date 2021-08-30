959fff69c653be71e2f0db7a25d39477b4944986
Сергей Еськов <sergey.yeskov@elmy.com>
2021-08-30 16:58:04 +0300

    fix typo

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onRequestPermissionsResult (int requestCode,String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult (int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case CLIENTBOOK_PERMISSIONS_REQUEST_SEND_SMS: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
